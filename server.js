02dfc42f0413feabc2939490b60222fbc6b71d89
Wellington Batista <wellingtonbs109@gmail.com>
2023-07-06 15:41:46 -0300

    feach: send email ok

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const express = require('express');
+const cors = require('cors');
 const nodemailer = require('nodemailer');
+
+const app = express();
+const port = 3002;
 
 // Configurar o transporte de email
 const transporter = nodemailer.createTransport({
@@ -11,13 +16,38 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-transporter.sendMail({
-  from: "Wellington <wellingtonbs109@gmail.com>",
-  to: "Wellington B <wellingtonbts10@gmail.com>",
-  subject: "Testando nodemailer",
-  text: "testando essa bagaça"
-}).then (message => {
-  console.log(message)
-}).catch ( err => {
-  console.log(err);
-})
+app.use(cors());
+app.use(express.json());
+
+// Rota para enviar e-mail
+app.post('/enviar-email', (req, res) => {
+
+  const nome = req.body.nome;
+  const from = req.body.from;
+  const to = req.body.to;
+  let texto = req.body.texto;
+
+  texto = `texto enviado por ${nome} de email ${from}: ${texto}`
+
+  const mailOptions = {
+    from: `${nome} <${from}>`,
+    to: to,
+    subject: "Contato",
+    text: texto
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Erro ao enviar e-mail' });
+    } else {
+      console.log('E-mail enviado: ' + info.response);
+      res.json({ message: 'E-mail enviado com sucesso' });
+    }
+  });
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +11,18 @@ export class HomePage {
 
   emailForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private alertController: AlertController) {
     this.emailForm = this.formBuilder.group({
       from: ['', Validators.required],
       to: ['wellingtonbs109@gmail.com'],
-      email: ['', Validators.required],
-      message: ['', Validators.required]
+      nome: ['', Validators.required],
+      texto: ['', Validators.required]
     });
   }
 
   sendEmail() {
+    console.log("send email");
+  
     if (this.emailForm.invalid) {
       return;
     }
@@ -27,26 +30,45 @@ export class HomePage {
     const emailData = {
       from: this.emailForm.value.from,
       to: 'wellingtonbs109@gmail.com',
-      email: this.emailForm.value.email,
-      message: this.emailForm.value.message
+      nome: this.emailForm.value.nome,
+      texto: this.emailForm.value.texto
     };
   
+    console.log(emailData);
+  
     // Fazer uma solicitação POST para o servidor
-    fetch('http://localhost:3000/send-email', {
+    fetch('http://localhost:3002/enviar-email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(emailData)
     })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data); // Mensagem de resposta do servidor
-    })
-    .catch(error => {
-      console.error(error); // Tratamento de erros
-    });
+      .then(response => response.text())
+      .then(async data => {
+        console.log(data); // Mensagem de resposta do servidor
+        this.emailForm.reset();
+        const alert = await this.alertController.create({
+          cssClass: "alert",
+          header: 'Email enviado com sucesso',
+          buttons: ['OK'],
+        });
+    
+        await alert.present();
+        
+      })
+      .catch(async error => {
+        console.error(error); // Tratamento de erros
+        const alert = await this.alertController.create({
+          cssClass: "alert",
+          header: 'Falha ao enviar email',
+          buttons: ['OK'],
+        });
+    
+        await alert.present();
+      });
   }
+  
   
 
 }
