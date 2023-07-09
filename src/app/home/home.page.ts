a9dc797bf2231d316524c269f10b7ce0bc74eb90
Wellington Batista <wellingtonbs109@gmail.com>
2023-07-08 21:01:43 -0300

    feach: add observer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,52 @@
-import { Component } from '@angular/core';
-import {register} from 'swiper/element/bundle'
-register();
+import { Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+import { register } from 'swiper/element/bundle';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChildren('observer') observerElements!: ElementRef[];
 
   constructor() { }
+
+  ngOnInit() {
+    this.registerSwiper();
+    this.observeContent();
+  }
+
+  ngAfterViewInit() {
+    this.observeContent();
+  }
 
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
     }
+  }
+
+  registerSwiper() {
+    register();
+  }
+
+  observeContent() {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade-in');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    this.observerElements?.forEach(elementRef => {
+      const element = elementRef.nativeElement;
+      if (element instanceof Element) {
+        observer.observe(element);
+      }
+    });
   }
 
 }
