c2c8b1bacd2b6d7024b67f42185a43bcdfe4c31d
T0ntow <wellingtonbs109@gmail.com>
2024-01-09 09:53:42 -0300

    update

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { ModalCourseComponent } from 'src/app/modal-course/modal-course.component';
+import { Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
@@ -8,12 +6,39 @@ import { ModalCourseComponent } from 'src/app/modal-course/modal-course.componen
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  @ViewChildren('observer') observerElements!: ElementRef[];
 
   constructor(
-    private modalCtrl: ModalController
   ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.observeContent();
+  }
+
+  observeContent() {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          console.log("sss");
+          
+          entry.target.classList.add('show');
+
+        } else {
+          entry.target.classList.remove('show');
+  
+        }
+      });
+    });
+  
+    this.observerElements?.forEach(elementRef => {
+      const element = elementRef.nativeElement;
+      if (element instanceof Element) {
+        observer.observe(element);
+      }
+    });
   }
 }
 
