7be4a6e97400773353cbfadb6f81239c7b501f5a
Wellington Batista <wellingtonbs109@gmail.com>
2024-01-12 18:47:12 -0300

    update

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -37,55 +37,64 @@ export class ProjectsComponent implements OnInit {
     });
     
     this.projetos[index].show = true;
-
+    this.selectedProject = this.projetos[index].name;
   }
 
-  selectedProject: string | null = null;
+  selectedProject: string | null = 'wallet-manager';
 
-  selectProject(projectName: string): void {
-    this.selectedProject = this.selectedProject === projectName ? null : projectName;
-  }
 
   swiperSlideChange(e: any) {
     // console.log("changed", e);
   }
 
-  imagesWallet = [
-    '/assets/images/wallet-manager/wallet-tablet-celular.png',
-    '/assets/images/wallet-manager/wallet-pc.png',
-  ]
-
-  imagesControleEstoque = [
-    '/assets/images/controle-estoque/controle-estoque.png',
-  ]
-
   projetos = [
     {
+      name: "wallet-manager",
       imagens: [ 
       '/assets/images/wallet-manager/wallet-tablet-celular.png',
       '/assets/images/wallet-manager/wallet-pc.png',],
-      tecnologias: ["logo-angular", "logo-ionic", "logo-firebase", "logo-angular"],
+      tecnologias: ["logo-angular", "logo-ionic", "logo-firebase"],
       descricao: "Aplicativo abrangente desenvolvido para gerenciar suas despesas em diversas carteiras, proporcionando uma experiência aprimorada. Inclui recursos gráficos e categorias que visam otimizar a organização e análise dos seus gastos",
       srcGithub: "",
       srcDeploy: "https://wallet-manager2.netlify.app/home",
       show: true
     },
     {
-      imagens: [],
-      tecnologias: [],
+      name: "controle-estoque",
+      imagens: [
+        '/assets/images/controle-estoque/controle-estoque.png', 
+      ],
+      tecnologias: ["logo-angular", "logo-ionic", 'logo-nodejs'],
+      srcTecnologias: ['/assets/images/servidor-sql.png'],
       descricao: "Aplicativo projetado para aprimorar minhas habilidades em SQL e NodeJS, ideal para a gestão eficiente de produtos em estoques de diversas empresas. Oferece uma ampla gama de recursos, incluindo diversos filtros e uma poderosa função de pesquisa para facilitar a navegação e a manipulação de dados",
       srcGithub: "https://github.com/T0ntow/Controle-Estoque",
       srcDeploy: "https://controle-estoque-sql.netlify.app/home",
       show: false
     },
-    // {
-    //   name: "projeto3",
-    //   imagens: [],
-    //   tecnologias: [],
-    //   descricao: "Aplicativo desenvolvido como projeto final da disciplina de Processos e Desenvolvimento de Sistemas, abrangendo todas as etapas desde a concepção no Figma até a implementação do projeto no VS Code. Essencial para compreender a utilidade dos processos ao longo do caminho até a conclusão",
-    //   srcGithub: "https://github.com/T0ntow/pet-club",
-    //   srcDeploy: "https://pet-club.netlify.app/home"
-    // },
+    {
+      name: "pet-club",
+      imagens: [
+        '/assets/images/pet/pet-club-pc.png',   
+        '/assets/images/pet/login-pet-club.png',   
+      ],
+      tecnologias: ["logo-angular", "logo-ionic", "logo-firebase", 'logo-nodejs'],
+      srcTecnologias: ['/assets/images/servidor-sql.png'],
+      descricao: "Aplicativo desenvolvido como projeto final da disciplina de Processos e Desenvolvimento de Sistemas, abrangendo todas as etapas desde a concepção no Figma até a implementação do projeto no VS Code. Essencial para compreender a utilidade dos processos ao longo do caminho até a conclusão",
+      srcGithub: "https://github.com/T0ntow/pet-club",
+      srcDeploy: "https://pet-club.netlify.app/home",
+      show: false
+    },
+    {
+      name: "git-api",
+      imagens: [ 
+        '/assets/images/github-explorer/git-hub-pc.png',   
+      ],
+      tecnologias: [],
+      descricao: "Aplicativo para enteder o padrão REST das APIs",
+      srcGithub: "https://github.com/T0ntow/pet-club",
+      srcDeploy: "https://github.com/T0ntow/github-api",
+      show: false
+    },
     
   ]
 
