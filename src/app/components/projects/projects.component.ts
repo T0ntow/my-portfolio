31a1ea110daaf774ada66623233617ba7dba23db
T0ntow <wellingtonbs109@gmail.com>
2024-01-12 11:50:18 -0300

    update

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -30,9 +30,20 @@ export class ProjectsComponent implements OnInit {
     this.swiper?.slidePrev();
   }
 
-  selectedProject: string = "projeto1";
-  toggleProject(project: string) {
-    this.selectedProject = project;
+
+  toggleProject(index: number): void {
+    this.projetos.forEach(element => {
+      element.show = false
+    });
+    
+    this.projetos[index].show = true;
+
+  }
+
+  selectedProject: string | null = null;
+
+  selectProject(projectName: string): void {
+    this.selectedProject = this.selectedProject === projectName ? null : projectName;
   }
 
   swiperSlideChange(e: any) {
@@ -46,6 +57,36 @@ export class ProjectsComponent implements OnInit {
 
   imagesControleEstoque = [
     '/assets/images/controle-estoque/controle-estoque.png',
+  ]
+
+  projetos = [
+    {
+      imagens: [ 
+      '/assets/images/wallet-manager/wallet-tablet-celular.png',
+      '/assets/images/wallet-manager/wallet-pc.png',],
+      tecnologias: ["logo-angular", "logo-ionic", "logo-firebase", "logo-angular"],
+      descricao: "Aplicativo abrangente desenvolvido para gerenciar suas despesas em diversas carteiras, proporcionando uma experiência aprimorada. Inclui recursos gráficos e categorias que visam otimizar a organização e análise dos seus gastos",
+      srcGithub: "",
+      srcDeploy: "https://wallet-manager2.netlify.app/home",
+      show: true
+    },
+    {
+      imagens: [],
+      tecnologias: [],
+      descricao: "Aplicativo projetado para aprimorar minhas habilidades em SQL e NodeJS, ideal para a gestão eficiente de produtos em estoques de diversas empresas. Oferece uma ampla gama de recursos, incluindo diversos filtros e uma poderosa função de pesquisa para facilitar a navegação e a manipulação de dados",
+      srcGithub: "https://github.com/T0ntow/Controle-Estoque",
+      srcDeploy: "https://controle-estoque-sql.netlify.app/home",
+      show: false
+    },
+    // {
+    //   name: "projeto3",
+    //   imagens: [],
+    //   tecnologias: [],
+    //   descricao: "Aplicativo desenvolvido como projeto final da disciplina de Processos e Desenvolvimento de Sistemas, abrangendo todas as etapas desde a concepção no Figma até a implementação do projeto no VS Code. Essencial para compreender a utilidade dos processos ao longo do caminho até a conclusão",
+    //   srcGithub: "https://github.com/T0ntow/pet-club",
+    //   srcDeploy: "https://pet-club.netlify.app/home"
+    // },
+    
   ]
 
 }
